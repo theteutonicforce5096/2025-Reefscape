70bf74b7d0873073e956242558e0b3fbcf25fa81
Jay <jay.ticku@gmail.com>
2025-04-01 18:54:55 -0500

    prob cleaned up

--- stubs/pyclipper/_version.pyi
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from builtins import object as VERSION_TUPLE
-__all__ = ['VERSION_TUPLE', 'version', 'version_tuple']
-__version__: str = '1.3.0.post6'
-__version_tuple__: tuple = (1, 3, 0)
-version: str = '1.3.0.post6'
-version_tuple: tuple = (1, 3, 0)
