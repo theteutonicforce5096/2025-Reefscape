2170f8322a52feea01119ac5d5f582e0c7524daf
Kellen Harris <Kellenbharris@icloud.com>
2025-04-01 20:47:59 -0500

    IT IS DONE

--- robot.py
+++ robot.py
@@ -10,35 +10,21 @@ class ReefscapeRobot(wpilib.TimedRobot):
         self.pxn_fightstick = wpilib.Joystick(2)
 
         
-        # Initialize drivetrain
-        # self.drivetrain = SwerveDrive()
-       
+     
         # Initialize LEDs
         self.lights = LED_controller()
         
         
 
-        # Initialize controllers
-        # self.drivetrain_controller = wpilib.XboxController(0)
-        # self.shooter_controller = wpilib.XboxController(1)
+      
 
         # Initialize timers
         self.timer = wpilib.Timer()
-        # self.drivetrain_timer = wpilib.Timer()
 
-        # Set default robot speeds
-        self.forward_speed = 0
-        self.strafe_speed = 0
-        self.rotation_speed = 0
 
     def teleopInit(self):
         
-        # self.lights.GETOUT()
-        # Reset timers
-        self.timer.restart()
-        # self.drivetrain_timer.reset()
-        
-        
+      
 
         # Calling to set the alliance color 
         alliance = wpilib.DriverStation.getAlliance()
@@ -50,104 +36,39 @@ class ReefscapeRobot(wpilib.TimedRobot):
         
             
         
-        self.forward_speed = 0
-        self.strafe_speed = 0
-        self.rotation_speed = 0
-        
-        # Reset Drivetrain
-        # self.drivetrain.reset_drivetrain()
-        # self.drivetrain.reset_gyro()
-
+       
     def teleopPeriodic(self):
-        # Get speeds from drivetrain controller.
-        # forward_speed = self.drivetrain_controller.getLeftY()
-        # strafe_speed = self.drivetrain_controller.getLeftX()
-        # rotation_speed = self.drivetrain_controller.getRightX()
-        
-        # Check if gyro needs to be reset.
-        # if self.drivetrain_controller.getAButtonPressed() and self.drivetrain_controller.getLeftBumperPressed() and self.drivetrain_controller.getRightBumperPressed():
-        #     self.drivetrain.stop_robot()
-        #     self.drivetrain.reset_gyro()
-        #     self.drivetrain_timer.restart()
-        #     self.drivetrain.change_drivetrain_state("Resetting Gyro")
-        
-        # Check if max drivetrain speed needs to be changed.
-        # if self.drivetrain_controller.getLeftTriggerAxis() > 0.1 and self.drivetrain_controller.getRightTriggerAxis() > 0.1:
-        #     self.drivetrain.change_max_drivetrain_speed(1.0)
-        # elif self.drivetrain_controller.getLeftTriggerAxis() > 0.1:
-        #     self.drivetrain.change_max_drivetrain_speed(0.5)
-        # elif self.drivetrain_controller.getRightTriggerAxis() > 0.1:
-        #     self.drivetrain.change_max_drivetrain_speed(0.75)
-        # else:
-        #     self.drivetrain.change_max_drivetrain_speed(0.25)
-
-        # Set deadzone on forward speed.
-        # if forward_speed > 0.1 or forward_speed < -0.1:
-        #     self.forward_speed = forward_speed
-        # else:
-        #     self.forward_speed = 0
-
-        # Set deadzone on strafe speed.
-        # if strafe_speed > 0.1 or strafe_speed < -0.1:
-        #     self.strafe_speed = strafe_speed
-        # else:
-        #     self.strafe_speed = 0
-
-        # Set deadzone on rotation speed.
-        # if rotation_speed > 0.1 or rotation_speed < -0.1:
-        #     self.rotation_speed = rotation_speed
-        # else:
-        #     self.rotation_speed = 0
-
-        # Evaluate drivetrain state.
-        # match self.drivetrain.get_drivetrain_state():
-        #     case "Enabled":
-        #         if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-        #             self.drivetrain.move_robot(self.forward_speed, self.strafe_speed, self.rotation_speed) 
-        #         else:
-        #             self.drivetrain.stop_robot()
-        #     case "Resetting Gyro":
-        #         if not self.drivetrain_timer.hasElapsed(0.5):
-        #             if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-        #                 self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0.75)  
-        #             else:
-        #                 self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
-        #         else:
-        #             self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
-        #             self.drivetrain_timer.reset()
-        #             self.drivetrain.change_drivetrain_state("Enabled")
-        #     case "Disabled":
-                # if self.forward_speed != 0 or self.strafe_speed != 0 or self.rotation_speed != 0:
-                #     self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0.75)
-                # else:
-                #     self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)
-        
-        # Coral Pickup
-        if self.pxn_fightstick.getRawButtonPressed(3):
-            self.lights.LED_CP(True)
-        # Algae Pickup
-        # if self.pxn_fightstick.getRawButtonPressed(4):
-        #     self.lights.LED_AP(True)
-        # Going Back to Alliance Color
-        if self.pxn_fightstick.getRawButtonPressed(2):
-            self.lights.LED_CP(False)
-            # self.lights.LED_AP(False)
-        # Hanging Animation            
+       
         if self.pxn_fightstick.getRawButtonPressed(1):
-            self.lights.LED_hang(True)
-        #Arm Angle 1
+            self.lights.LED_CP
+            # Ingest
+
+        if self.pxn_fightstick.getRawButtonPressed(2):
+            self.lights.LED_CE
+            # Expel
+
         if self.pxn_fightstick.getRawButtonPressed(5):
-            self.lights.LED_AA1(True)
-        #Arm Angle 2
+            self.lights.LED_L1
+            # Level 1
+
+        if self.pxn_fightstick.getRawButtonPressed(3):
+            self.lights.LED_L2
+            # Level 2
+
+        if self.pxn_fightstick.getRawButtonPressed(4):
+            self.lights.LED_L3
+            # Level 3
+
         if self.pxn_fightstick.getRawButtonPressed(6):
-            self.lights.LED_AA2(True)
-        #Arm Angle 3
-        if self.pxn_fightstick.getRawButtonPressed(7):
-            self.lights.LED_AA3(True)
-    
+            self.lights.LED_L4
+            # Level 4
+
+        if self.pxn_fightstick.getRawButtonPressed(9):
+            self.lights.LED_CLIMB
+            # Climb
+
     def teleopExit(self):
-        # Turn off drivetrain controller rumble if it is stil on.
-        # self.drivetrain_controller.setRumble(wpilib.XboxController.RumbleType.kBothRumble, 0)  
+       
         pass
 
 if __name__ == "__main__":
--- subsystems/LED_controller.py
+++ subsystems/LED_controller.py
@@ -14,7 +14,7 @@ from phoenix5.led import TwinkleOffAnimation
 
 class LED_controller:
 
-    # What controls the color of the LED
+   # What controls the color of the LED
     def __init__(self):
         self.candle = phoenix5.led.CANdle(1)
         self.candle.configFactoryDefault()
@@ -40,67 +40,33 @@ class LED_controller:
 
     #   ^^^ The programmers of the py library made a bug to phoenix5 SO
     #   ^^^ to clear the animation you have to redeploy THIS
-    def LED_CP(self, have_coral: bool):
-        # LED Coral Pickup Color
-        if have_coral:
-            self.candle.setLEDs(200, 10, 20, 0, 154, 154)
-        else:
-            self.shine_alliance_color()
-
-    def LED_hang(self, have_hang: bool):
-        # LED robot hang Color
-        if have_hang:
-            self.candle.animate(
-                LarsonAnimation(
-                    0,
-                    150,
-                    50,
-                    0,
-                    0.75,
-                    77,
-                    phoenix5.led.LarsonAnimation.BounceMode(0),
-                    7,
-                    154,
-                ),
-                0,
-            )
-            self.candle.animate(
-                LarsonAnimation(
-                    0,
-                    150,
-                    50,
-                    0,
-                    0.75,
-                    77,
-                    phoenix5.led.LarsonAnimation.BounceMode(0),
-                    7,
-                    231,
-                ),
-                1,
-            )
-        else:
-            self.shine_alliance_color()
-
-    def LED_AA1(self, have_angle1: bool):
-        # LED robot arm angle Color
-        if have_angle1:
-            self.candle.setLEDs(225, 225, 225, 0, 154, 154)
-        else:
-            self.alliance_color()
-
-    def LED_AA2(self, have_angle2: bool):
-        # LED robot arm angle Color
-        if have_angle2:
-            self.candle.setLEDs(10, 225, 0, 0, 154, 154)
-        else:
-            self.alliance_color()
-
-    def LED_AA3(self, have_angle3: bool):
-        # LED robot arm angle Color
-        if have_angle3:
-            self.candle.setLEDs(225, 0, 225, 0, 154, 154)
-        else:
-            self.alliance_color()
+        
+    def LED_CP(self):
+        self.candle.setLEDs(200, 10, 20, 0, 154, 154) 
+        "Pink Color"
+    
+    def LED_CE(self):
+        self.candle.setLEDs(0, 110, 225, 0, 154, 154) 
+        "Teal Color"
+    def LED_CLIMB(self):
+        self.candle.animate(LarsonAnimation(0,150,50,0,0.75,77,phoenix5.led.LarsonAnimation.BounceMode(0),7,154,),0,)
+        "Yellow Animation"
+    
+    def LED_L1(self): 
+        self.candle.setLEDs(225, 225, 225, 0, 154, 154)
+        "White Color"
+    
+    def LED_L2(self):
+        self.candle.setLEDs(10, 225, 0, 0, 154, 154)
+        "Green Color"
+    
+    def LED_L3(self):
+        self.candle.setLEDs(225, 0, 225, 0, 154, 154)
+        "Purple Color"
+    
+    def LED_L4(self):
+        self.candle.setLEDs(225, 123, 21, 0, 154, 154)
+        "Orange Color"
 
     def set_alliance_color(self, isRed: bool):
         if isRed:
@@ -115,17 +81,18 @@ class LED_controller:
         else:
             self.candle.setLEDs(0, 0, 100, 0, 0, 308)
 
-    # +!!!  ANIMATIONS  !!!+
-    # A bunch of animations that we have !!! we can import more if we have to
-    # We might also want to create custom animations
 
-    # self.candle.animate(LarsonAnimation(0,200,50,0,45,-1,phoenix5.led.LarsonAnimation.BounceMode(2),2,0))
-    # self.candle.animate(StrobeAnimation(5,5,50,0,1,82,0))
-    # self.candle.animate(ColorFlowAnimation(1,1,225,0,.25,308,phoenix5.led.ColorFlowAnimation.Direction(0),0))
-    # self.candle.animate(FireAnimation(.80,.5,200,.35,.35,True,0))
-    # self.candle.animate(SingleFadeAnimation(0,100,100,0,.5,308,0))
-    # self.candle.animate(RgbFadeAnimation(.89,.5,-1,0))
-    # self.candle.animate(TwinkleAnimation(100,0,100,0,.5,-1,phoenix5.led.TwinkleAnimation.TwinklePercent(100),0))
-    # self.candle.animate(TwinkleOffAnimation(100,100,100,0,.5,-1,phoenix5.led.TwinkleOffAnimation.TwinkleOffPercent(100),0))
-    # self.candle.animate(RainbowAnimation(.87,.5,-1,False,0))
-    # self.candle.animate(ColorFlowAnimation(0,200,40,0,.5,154,phoenix5.led.ColorFlowAnimation.Direction(1),0))
+    # # +!!!  ANIMATIONS  !!!+
+    # # A bunch of animations that we have !!! we can import more if we have to
+    # # We might also want to create custom animations
+
+    # # self.candle.animate(LarsonAnimation(0,200,50,0,45,-1,phoenix5.led.LarsonAnimation.BounceMode(2),2,0))
+    # # self.candle.animate(StrobeAnimation(5,5,50,0,1,82,0))
+    # # self.candle.animate(ColorFlowAnimation(1,1,225,0,.25,308,phoenix5.led.ColorFlowAnimation.Direction(0),0))
+    # # self.candle.animate(FireAnimation(.80,.5,200,.35,.35,True,0))
+    # # self.candle.animate(SingleFadeAnimation(0,100,100,0,.5,308,0))
+    # # self.candle.animate(RgbFadeAnimation(.89,.5,-1,0))
+    # # self.candle.animate(TwinkleAnimation(100,0,100,0,.5,-1,phoenix5.led.TwinkleAnimation.TwinklePercent(100),0))
+    # # self.candle.animate(TwinkleOffAnimation(100,100,100,0,.5,-1,phoenix5.led.TwinkleOffAnimation.TwinkleOffPercent(100),0))
+    # # self.candle.animate(RainbowAnimation(.87,.5,-1,False,0))
+    # # self.candle.animate(ColorFlowAnimation(0,200,40,0,.5,154,phoenix5.led.ColorFlowAnimation.Direction(1),0))
