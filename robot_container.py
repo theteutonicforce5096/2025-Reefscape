b1ecee0842653208f9e19686d5377d18c82c419c
Jay <jay.ticku@gmail.com>
2025-05-27 20:48:51 -0500

    Update robot_container.py

--- robot_container.py
+++ robot_container.py
@@ -9,6 +9,7 @@ class RobotContainer:
         self.wrist = Wrist(50)
 
         self.controller = commands2.button.CommandXboxController(0)
+        self.elevator_controller = commands2.button.CommandJoystick(1)
         
     def configure_button_bindings_teleop(self):
         self.wrist.setDefaultCommand(
@@ -22,6 +23,26 @@ class RobotContainer:
         self.controller.povDown().whileTrue(
             self.elevator.run(lambda: self.elevator.lower_setpoint_small())
         )
+        
+        self.elevator_controller.button(5).onTrue(
+            self.elevator.runOnce(lambda: self.elevator.set_setpoint(0))
+        )
+        # 1'1" =]
+        
+        self.elevator_controller.button(3).onTrue(
+            self.elevator.runOnce(lambda: self.elevator.set_setpoint(24.78))
+        )
+        # 2'7.875" =3
+        
+        self.elevator_controller.button(4).onTrue(
+            self.elevator.runOnce(lambda: self.elevator.set_setpoint(57))
+        )
+        # 3'11.625" :OOO
+        
+        self.elevator_controller.button(6).onTrue(
+            self.elevator.runOnce(lambda: self.elevator.set_setpoint(95))
+        )
+        # 6' =]]]]]]
 
         # self.controller.povRight().onTrue(
         #     self.elevator.run(lambda: self.elevator.raise_setpoint())
