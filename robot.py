cd6825c1e95c15dffc03cbaa49ba8f4ec8fa7f18
Kellen Harris <Kellenbharris@icloud.com>
2025-06-03 19:36:53 -0500

    I'm not exactly sure what happened but the modules are messed up and I want to save my code :((((

--- robot.py
+++ robot.py
@@ -1,11 +1,14 @@
 import wpilib
 from subsystems.Coral_Manipulator import coral_manipulator
+from networktables import NetworkTables
+
 
 #from subsystems.drivetrain import SwerveDrive
 
 class ReefscapeRobot(wpilib.TimedRobot):
     def robotInit(self):
        self.goodStick = wpilib.XboxController(1)
+       self.pxn_fightstick = wpilib.Joystick(0)
        self.coral_manipulator = coral_manipulator 
 
     def teleopInit(self):
@@ -13,7 +16,8 @@ class ReefscapeRobot(wpilib.TimedRobot):
     
 
     def teleopPeriodic(self):
-        pass
+        if self.pxn_fightstick.getRawButtonPressed(1):
+            self.coral_manipulator.intake()
        
     def teleopExit(self):
         pass
--- subsystems/Coral_Manipulator.py
+++ subsystems/Coral_Manipulator.py
@@ -1,7 +1,5 @@
 import wpilib
-from wpilib import Servo
 from wpilib import Joystick
-import wpimath.units
 from wpilib import Timer
 import rev
 from networktables import NetworkTables
@@ -14,6 +12,8 @@ from wpimath.trajectory import TrapezoidProfile
 class coral_manipulator:
    
     def __init__(self):
+        
+        #Configuring Motor
         self.sucker_config = rev.SparkBaseConfig
         self.sucker = rev.SparkMax(
             50, rev.SparkMax.MotorType.kBrushless
@@ -23,5 +23,15 @@ class coral_manipulator:
             rev.SparkMax.ResetMode.kNoResetSafeParameters,
             rev.SparkMax.PersistMode.kNoPersistParameters,
         )
+        #Configuring Timer
+        self.intake_timer = wpilib.Timer
+
+
     def intake(self):
         self.sucker.set(.25)
+        self.intake_timer.restart
+        if self.intake_timer.hasElapsed(True, 3):
+            self.sucker.set(0)
+
+    def stop(self):
+        self.sucker.set(0)
